b9d69e4d7bccfa7bba067ab945d8eba9b63ec9b0
Devin <devin.biggers@gmail.com>
2020-06-02 15:42:21 -0500

    (bigz) Updating for heroku app

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 
-app.use(express.static(__dirname + '/dist/tictoctoe'));
+app.use(express.static(__dirname + '/dist/'));
 
 // Start the app by listening on the default
 // Heroku port
