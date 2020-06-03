368af89ecbe31d9d987765e9683c2ced2b97fc5a
Devin <devin.biggers@gmail.com>
2020-06-02 23:12:16 -0500

    (bigz) Updating for heroku app

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const path = require ('path');
 
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/tictoctoe'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'/dist/tictoctoe/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'))
-})
-
-console.log('Console listening!');
-
